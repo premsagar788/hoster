6f4bcf2e203522f4f0f3f52e665152e5ba77d5ae
Prem Sagar <prem_saagar@ymail.com>
2022-11-29 21:31:52 +0500

    update

--- app/Http/Controllers/DomainsController.php
+++ app/Http/Controllers/DomainsController.php
@@ -14,11 +14,8 @@ class DomainsController extends Controller
 
     public function check(Request $request)
     {
-        $domain = $request->input('domain');
+        $domain = $this->validateDomain($request->input('domain'));
 
-        if(!$domain) {
-            return redirect()->back()->withErrors('No domain entered');
-        }
         if (gethostbyname($domain) != $domain ) {
             $status = 'Unavailable';
         }
@@ -28,9 +25,47 @@ class DomainsController extends Controller
         return view('domains')->with(compact('status', 'domain'));
     }
 
+    private function validateDomain($domain): ?string
+    {
+        $domain = trim($domain);
+        
+        if (substr(strtolower($domain), 0, 7) == "http://"){
+            $domain = substr($domain, 7);
+        } elseif (substr(strtolower($domain), 0, 8) == "https://") {
+            $domain = substr($domain, 8);
+        } elseif (substr(strtolower($domain), 0, 4) == "www.") {
+            $domain = substr($domain, 4);
+        }
+
+        if(!$domain) {
+            return redirect()->back()->withErrors('No domain entered');
+        }
+
+        return $domain;
+    }
+
     public function whois($domain)
     {
-        dd($domain);
+        $domain = $this->validateDomain($domain);
+        return $this->DNSRecords($domain);
+        // $whois = dns_get_record($domain, DNS_ANY);
+    }
+
+    private function DNSRecords($domain)
+    {
+        $aRec = dns_get_record($domain, DNS_A);
+        $cname = dns_get_record($domain, DNS_CNAME);
+        $hInfo = dns_get_record($domain, DNS_HINFO);
+        $NAPtr = dns_get_record($domain, DNS_NAPTR);
+        $MX = dns_get_record($domain, DNS_MX);
+        $NS = dns_get_record($domain, DNS_NS);
+        $PTR = dns_get_record($domain, DNS_PTR);
+        $SOA = dns_get_record($domain, DNS_SOA);
+        $TXT = dns_get_record($domain, DNS_TXT);
+        $AAAA = dns_get_record($domain, DNS_AAAA);
+        $SRV = dns_get_record($domain, DNS_SRV);
+
+        return view('whois')->with(compact('aRec', 'cname', 'hInfo', 'NAPtr', 'MX', 'NS', 'PTR', 'SOA', 'TXT', 'AAAA', 'SRV'));
     }
 
     public function addToCart($domain)
