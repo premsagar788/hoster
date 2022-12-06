c34e9ee6841340b873159d5aa9f2fad7050f1d23
Prem Sagar <prem_saagar@ymail.com>
2022-12-06 20:06:50 +0500

    indentation fix

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\User;
+use Auth;
 use Validator;
 use Socialite;
 use Exception;
-use Auth;
+use App\Models\User;
+use Illuminate\Http\Request;
 class SocialController extends Controller
 {
     public function googleRedirect()
     {
         return Socialite::driver('google')->redirect();
     }
+    
     public function loginWithGoogle()
     {
         try {
@@ -21,10 +22,10 @@ class SocialController extends Controller
             $user = Socialite::driver('google')->stateless()->user();
             $existingUser = User::where('google_id', $user->id)->first();
      
-            if($existingUser){
+            if ($existingUser) {
                 Auth::login($existingUser);
                 return redirect('/home');
-            }else{
+            } else {
                 $createUser = User::create([
                     'name' => $user->name,
                     'email' => $user->email,
