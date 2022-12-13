50b03ed346c1f9f47683308416f31cde415fe386
Prem Sagar <prem_saagar@ymail.com>
2022-12-13 11:35:41 +0500

    created orders model

---
+++ app/Models/Order.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $table = 'orders';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'product',
+		'price',
+		'payment_method',
+		'user_id',
+		'status'
+    ];
+}
