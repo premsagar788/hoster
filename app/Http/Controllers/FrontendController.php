3d14ce82b35870f94694e0ed03028f65659aceca
Prem Sagar <prem_saagar@ymail.com>
2022-12-13 21:53:23 +0500

    updated Order details

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
@@ -14,5 +15,16 @@ class FrontendController extends Controller
     public function checkout()
     {
     	return view('checkout');
+    }
+
+    public function processCheckout(Request $request)
+    {
+    	$cart = session('cart');
+    	foreach ($cart as $key) {
+    		dd($key['name']);
+    	}
+    	$order = new Order();
+    	
+    	dd($request->all());
     }
 }
