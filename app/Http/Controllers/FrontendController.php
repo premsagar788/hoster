d09e20f45c7097492ff30d682d60254e915f69db
Prem Sagar <prem_saagar@ymail.com>
2022-12-15 14:28:17 +0500

    create order

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Order;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
+	protected $success;
+
+	public function __construct()
+    {
+        $this->success;
+    }
+
     public function cart()
     {
         return view('cart');
@@ -19,12 +27,33 @@ class FrontendController extends Controller
 
     public function processCheckout(Request $request)
     {
-    	$cart = session('cart');
-    	foreach ($cart as $key) {
-    		dd($key['name']);
+		$user = User::find(auth()->user()->id);
+    	$user->name = $request->input('name');
+    	$user->address = $request->input('address');
+    	$user->city = $request->input('city');
+    	$user->country = $request->input('country');
+    	$user->phone = $request->input('phone');
+    	$user->save();
+
+    	foreach (session('cart') as $key) {
+	    	$order = new Order();
+	    	
+	    	$order->product = $key['name'];
+	    	$order->quantity = $key['quantity'];
+	    	$order->price = $key['price'];
+	    	$order->payment_method = $request->input('payment');
+	    	$order->user_id = $user->id;
+	    	$order->save();
+	    	$this->success = true;
     	}
-    	$order = new Order();
-    	
-    	dd($request->all());
+    	if ($this->isSuccess()) {
+    		session()->forget('cart');
+    		return redirect('/')->with('success', 'Order placed successfully!');   
+    	}
+    }
+
+    private function isSuccess(): bool
+    {
+    	return $this->success ? true : false;
     }
 }
