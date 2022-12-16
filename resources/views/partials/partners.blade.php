93f688d601da8536cdd882895aabe66a13346685
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2022-12-16 11:09:45 +0500

    Develop (#18)
    * indentation improved
    
    * User account
    
    * indentation fix
    
    * User dashboard (#12)
    
    * Develop (#9)
    
    * indentation improved
    
    * User account
    
    * indentation fix
    
    * Roles perms (#10)
    
    * role middleware added
    
    * ui fix ~ login page
    
    * login/register ui fix
    
    * Develop (#11)
    
    * indentation improved
    
    * User account
    
    * indentation fix
    
    * user dashboard
    
    * user dashboard update
    
    * role based redirect
    
    * added role @ register
    
    Signed-off-by: Prem Sagar <50700219+premsagar788@users.noreply.github.com>
    
    * Order (#15)
    
    * user dashboard
    
    * user dashboard update
    
    * role based redirect
    
    * added role @ register
    
    * admin dashboard views
    
    * dashboard ui fixes
    
    * spacing
    
    * dns fixes
    
    * created orders table
    
    * improved whois ui
    
    * created orders controller
    
    * created orders model
    
    * ui fixes in footer
    
    * added route for checkout
    
    * function for checkout
    
    * separated partners section
    
    * added checkout page
    
    * updated Order details
    
    * updated user model
    
    Updated user model and synced with checkout
    
    * fix migration issue
    
    * fix migration issue
    
    * updated users table
    
    Updated users table according to checkout and added fields
    
    * checkout ui fixes
    
    improved checkout ui page, removed redundant code
    
    * update checkout
    
    updated checkout and create an order
    
    * create order
    
    * updated orders table
    
    updated status type, added addidtional notes column and quantity in orders table
    
    * updated checkout ui
    
    Signed-off-by: Prem Sagar <50700219+premsagar788@users.noreply.github.com>
    
    * Order (#17)
    
    * user dashboard
    
    * user dashboard update
    
    * role based redirect
    
    * added role @ register
    
    * admin dashboard views
    
    * dashboard ui fixes
    
    * spacing
    
    * dns fixes
    
    * created orders table
    
    * improved whois ui
    
    * created orders controller
    
    * created orders model
    
    * ui fixes in footer
    
    * added route for checkout
    
    * function for checkout
    
    * separated partners section
    
    * added checkout page
    
    * updated Order details
    
    * updated user model
    
    Updated user model and synced with checkout
    
    * fix migration issue
    
    * fix migration issue
    
    * updated users table
    
    Updated users table according to checkout and added fields
    
    * checkout ui fixes
    
    improved checkout ui page, removed redundant code
    
    * update checkout
    
    updated checkout and create an order
    
    * create order
    
    * updated orders table
    
    updated status type, added addidtional notes column and quantity in orders table
    
    * updated checkout ui
    
    * indentation fixes
    
    Signed-off-by: Prem Sagar <50700219+premsagar788@users.noreply.github.com>
    
    Signed-off-by: Prem Sagar <50700219+premsagar788@users.noreply.github.com>

--- resources/views/partials/partners.blade.php
+++ resources/views/partials/partners.blade.php
@@ -1,31 +1,31 @@
 <section class="partners">
-  <div class="container">
-    <h2 class="text-center text-white">Our Partners</h2>
-    <br>
-    <div class="row">
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo1.png') }}">
-      </div>
+	<div class="container">
+		<h2 class="text-center text-white">Our Partners</h2>
+		<br>
+		<div class="row">
+			<div class="col col-lg-2 col-md-6 col-sm-12">
+				<img src="{{ asset('/images/logo1.png') }}">
+			</div>
 
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo2.png') }}">
-      </div>
+			<div class="col col-lg-2 col-md-6 col-sm-12">
+				<img src="{{ asset('/images/logo2.png') }}">
+			</div>
 
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo3.png') }}">
-      </div>
+			<div class="col col-lg-2 col-md-6 col-sm-12">
+				<img src="{{ asset('/images/logo3.png') }}">
+			</div>
 
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo4.png') }}">
-      </div>
+			<div class="col col-lg-2 col-md-6 col-sm-12">
+				<img src="{{ asset('/images/logo4.png') }}">
+			</div>
 
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo5.png') }}">
-      </div>
+			<div class="col col-lg-2 col-md-6 col-sm-12">
+				<img src="{{ asset('/images/logo5.png') }}">
+			</div>
 
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo6.png') }}">
-      </div>
-    </div>
-  </div>
+			<div class="col col-lg-2 col-md-6 col-sm-12">
+				<img src="{{ asset('/images/logo6.png') }}">
+			</div>
+		</div>
+	</div>
 </section>
