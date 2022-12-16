9211e71652d20a888b57a20dba1720df9c272a86
Prem Sagar <prem_saagar@ymail.com>
2022-12-16 12:15:13 +0500

    navbar responsiveness fixed

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -1,84 +1,101 @@
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.84.0">
-    <title>@yield('title')</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.84.0">
+  <title>@yield('title')</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/carousel/">
+  <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/carousel/">
 
-    <!-- Bootstrap core CSS -->
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+  <!-- Bootstrap core CSS -->
+  <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 
-    <meta name="theme-color" content="#7952b3">
-    <meta charset = "UTF-8" />
+  <meta name="theme-color" content="#7952b3">
+  <meta charset = "UTF-8" />
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
       }
+    }
+  </style>
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
 
-    
-    <!-- Custom styles for this template -->
-    <link href="{{ asset('css/carousel.css') }}" rel="stylesheet">
-  </head>
-  <body>
+  <!-- Custom styles for this template -->
+  <link href="{{ asset('css/carousel.css') }}" rel="stylesheet">
+</head>
+<body>
+  <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
-      <header>
-        <div class="d-flex flex-column flex-md-row align-items-center border-bottom">
-          <a href="{{ url('/') }}" class="d-flex align-items-center pt-3 pb-3 text-dark text-decoration-none">
-            <span class="fs-4">Hosting</span>
-          </a>
+      <a class="navbar-brand" href="http://127.0.0.1:8000">
+        Hosting
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto">
+        </ul>
 
-          <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-            <a class="me-3 py-2 text-dark text-decoration-none" href="{{ url('about') }}">About</a>
-            <a class="me-3 py-2 text-dark text-decoration-none" href="{{ url('domains') }}">Domains</a>
-            <a class="me-3 py-2 text-dark text-decoration-none" href="{{ url('pricing') }}">Pricing</a>
-            <a class="me-3 py-2 text-dark text-decoration-none" href="{{ url('faq') }}">FAQ</a>
-            @auth
-              <a class="py-2 btn" href="{{ url('my-account') }}">
-                <span class="fa fa-user"></span>
-              </a>
-            @endauth
-            @guest
-              <a class="py-2 btn btn-primary" href="{{ url('login') }}">Login</a>
-            @endguest
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ms-auto">
+          <!-- Authentication Links -->
+
+          <li class="nav-item">
+            <a class="nav-link" href="{{ url('about') }}">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ url('domains') }}">Domains</a>
+          </li>
+          <li class="nav-item">         
+            <a class="nav-link" href="{{ url('pricing') }}">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ url('faq') }}">FAQ</a>
+          </li>
+          @auth
+          <a class="py-2 btn" href="{{ url('my-account') }}">
+            <span class="fa fa-user"></span>
+          </a>
+          @endauth
+          @guest
+          <a class="py-2 btn btn-primary" href="{{ url('login') }}">Login</a>
+          @endguest
+          <li>
             <a class="me-2 text-dark text-decoration-none" href="{{ url('/cart') }}"><i class="fa fa-shopping-cart"></i> 
               <span class="cart-count">{{ count((array) session('cart')) }}</span>
             </a>
-          </nav>
-        </div>
-      </header>
+          </li>
+        </ul>
+      </div>
     </div>
-    
-    <main>
-      @yield('content')
-        <!-- FOOTER -->
-        <section class="footer-section">
-          <footer class="container">
-            <p class="float-end"><a href="#">Back to top</a></p>
-            <p>© 2017–2021 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
-          </footer>
-        </section>
-      
-    </main>
+  </nav>
+  <main>
+    @yield('content')
+    <!-- FOOTER -->
+    <section class="footer-section">
+      <footer class="container">
+        <p class="float-end"><a href="#">Back to top</a></p>
+        <p>© 2017–2021 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+      </footer>
+    </section>
 
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+  </main>
 
-  </body>
+  <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+
+</body>
 </html>
