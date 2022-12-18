3a6b0c2ac2fa0e26be95bc0767278eb1c84e668b
Prem Sagar <prem_saagar@ymail.com>
2022-12-18 13:58:53 +0500

    separated sidebar

---
+++ resources/views/user/sidebar.blade.php
@@ -0,0 +1,38 @@
+@section('sidebar')
+
+<!-- <li class="nav-item">
+	<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+	aria-expanded="true" aria-controls="collapseTwo">
+	<i class="fas fa-fw fa-cog"></i>
+	<span>Components</span>
+</a>
+<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+	<div class="bg-white py-2 collapse-inner rounded">
+		<h6 class="collapse-header">Custom Components:</h6>
+		<a class="collapse-item" href="buttons.html">Buttons</a>
+		<a class="collapse-item" href="cards.html">Cards</a>
+	</div>
+</div>
+</li> -->
+
+<li class="nav-item">
+	<a class="nav-link" href="{{ url(auth()->user()->role . '/orders') }}">
+		<i class="fas fa-fw fa-shopping-cart"></i>
+		<span>Orders</span>
+	</a>
+</li>
+
+<li class="nav-item">
+	<a class="nav-link" href="{{ url(auth()->user()->role . '/invoices') }}">
+		<i class="fas fa-fw fa-file"></i>
+		<span>Invoices</span>
+	</a>
+</li>
+
+<li class="nav-item">
+	<a class="nav-link" href="{{ url(auth()->user()->role . '/profile') }}">
+		<i class="fas fa-fw fa-user"></i>
+		<span>Profile</span>
+	</a>
+</li>
+@endsection
