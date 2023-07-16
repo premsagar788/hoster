25f61e873a57dcfb2c3d99ef14fe5dff3a00f56d
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2023-07-16 13:39:12 +0500

    declutter

--- database/migrations/2022_12_13_042155_create_orders_table.php
+++ database/migrations/2022_12_13_042155_create_orders_table.php
@@ -15,9 +15,6 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('product');
-            $table->unsignedBigInteger('quantity');
-            $table->string('price');
             $table->string('payment_method');
             $table->unsignedBigInteger('user_id');
             $table->enum('status', ['Pending', 'Cancelled', 'Paid'])->default('Pending');
