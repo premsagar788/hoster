6b69d73735a3aaf9e1a1d3bbde23e5b8e1b77b3a
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2023-07-16 13:07:59 +0500

    added order details tbl

---
+++ database/migrations/2023_07_16_074929_create_order_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_id', 50);
+            $table->float('price', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+}
