f44019063bff7cad64d9476ea64d7b0fbb0a5374
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2023-07-16 14:09:33 +0500

    added profile view

---
+++ resources/views/admin/profile.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Admin Dashboard')
+
+@section('content')
+	<!-- Begin Page Content -->
+	<div class="container-fluid">
+
+		<!-- Page Heading -->
+		<div class="d-sm-flex align-items-center justify-content-between mb-4">
+			<h1 class="h3 mb-0 text-gray-800">Profile</h1>
+		</div>
+
+		<!-- Content Row -->
+		<div class="row">
+
+
+		</div>
+	</div>
+	<!-- /.container-fluid -->
+@endsection
+
+<script src="{{ asset('js/demo/chart-area-demo.js') }}"></script>
+<script src="{{ asset('js/demo/chart-pie-demo.js') }}"></script>
