0815821843f23d8b027f624f51aec173d50a1d18
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2023-08-19 01:17:50 +0500

    cleanup and delete cart
    cleanup of add to cart feature and added feature to delete product from cart using AJAX

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -2,7 +2,13 @@
 
 @section('title', 'Cart')
 
+@section('head')
+<meta name="csrf-token" content="{{ csrf_token() }}" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+@endsection
+
 @section('content')
+<?php $total = 0 ?>
 
 <div class="jumbotron">
   <div class="container">
@@ -10,6 +16,8 @@
     <p>Review your cart before checking out!</p>
   </div>
 </div>
+
+@if(session('cart'))
 <div class="container cart">
   <div class="row">
     <table id="cart" class="table table-hover table-bordered">
@@ -24,26 +32,25 @@
       </thead>
       <tbody> 
         @if(session('cart') !== null)
-        @foreach(session('cart') as $product)
+        @foreach($cart as $product)
+        <?php $total = $total + (double) $product['price'] * (int) $product['quantity']; ?>
         <tr>
           <td data-th="Product">
             <div class="row">
               <div class="col-sm-3 hidden-xs">
                 <p>{{ $product['name'] }}</p>
               </div>
-              <div class="col-sm-9">
-                
-              </div>
+              <div class="col-sm-9"></div>
             </div>
           </td>
-          <td data-th="Price">$11.25</td>
+          <td data-th="Price">{{ $product['price'] }}</td>
           <td data-th="Quantity">
-            <input type="number" value="1" class="form-control quantity">
+            <input type="number" value="{{ $product['quantity'] }}" class="form-control quantity">
           </td>
-          <td data-th="Subtotal" class="text-center">$11.25</td>
+          <td data-th="Subtotal" class="text-center">{{ $product['price'] * $product['quantity'] }}</td>
           <td class="actions" data-th="">
             <button class="btn btn-info btn-sm update-cart" data-id="testddd.com"><i class="fa fa-refresh"></i></button>
-            <button class="btn btn-danger btn-sm remove-from-cart" data-id="testddd.com"><i class="fa fa-trash-o"></i></button>
+            <button id="delete" class="btn btn-danger btn-sm remove-from-cart" value="{{ $product['name'] }}"><i class="fa fa-trash-o"></i></button>
           </td>
         </tr>
         @endforeach
@@ -51,7 +58,7 @@
       </tbody>
       <tfoot>
         <tr class="visible-xs">
-          <td class="text-center"><strong>Total 11.25</strong></td>
+          <td class="text-center"><strong>0</strong></td>
           <td>
             <button>Checkout</button>
           </td>
@@ -61,7 +68,7 @@
             <a href="#" class="btn btn-warning"> Continue Shopping</a>
           </td>
           <td colspan="2" class=""></td>
-          <td class="hidden-xs text-center"><strong>Total $11.25</strong></td>
+          <td class="hidden-xs text-center"><strong>Total ${{ $total }}</strong></td>
           <td>
             <a href="checkout" class="btn btn-success" style="color:white; text-decoration: none;">
             Checkout</a>
@@ -71,36 +78,40 @@
     </table>
   </div>
 </div>
-<section class="partners">
-  <div class="container">
-    <h2 class="text-center text-white">Our Partners</h2>
-    <br>
-    <div class="row">
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo1.png') }}">
-      </div>
 
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo2.png') }}">
-      </div>
+@else
 
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo3.png') }}">
-      </div>
+    <h1 align="center"><i class="fa fa-trash-o"></i></h1>
+    <h3 align="center" class="pb-5">Oh no! Looks like empty Cart!</h3>
 
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo4.png') }}">
-      </div>
+@endif
 
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo5.png') }}">
-      </div>
+@include('partials.partners')
 
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo6.png') }}">
-      </div>
-    </div>
-  </div>
-</section>
+<script type="text/javascript">
+   
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+   
+    $("#delete").click(function(e){
+  
+        e.preventDefault();
+
+        let domain = $("#delete").val();
+   
+        $.ajax({
+           type:'POST',
+           url:"{{ url('delete') }}"+ '/' +domain,
+           data:{domain:domain},
+           success:function(data){
+              alert(data.success);
+              location.reload();
+           }
+        });
+    });
+</script>
 
 @endsection
