55ea3e62f3f48474457ccd7e603f3ae192285f05
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2023-12-17 13:03:19 +0500

    pricing partial added

---
+++ resources/views/partials/pricing.blade.php
@@ -0,0 +1,77 @@
+<section class="pricing-section">
+	<h2 class="text-center">Plan and Pricing</h2>
+	<p class="text-center">Cum sociis natoque penatibus et magnis dis parturient montes, nasceturridiculus mus.donequam felis, ultricies nec, pellentesque eu.</p>
+	<br>
+	<div class="container">
+		<div class="row row-cols-1 row-cols-md-4 mb-4 text-center">
+			<div class="col">
+				<div class="card mb-4 rounded-3 shadow-sm">
+					<div class="card-header py-3">
+						<h4 class="my-0 fw-normal">Free</h4>
+					</div>
+					<div class="card-body">
+						<h1 class="card-title pricing-card-title">$0<small class="text-muted fw-light">/mo</small></h1>
+						<ul class="list-unstyled mt-3 mb-4">
+							<li>10 users included</li>
+							<li>2 GB of storage</li>
+							<li>Email support</li>
+							<li>Help center access</li>
+						</ul>
+						<button type="button" class="w-100 btn-lg btn-outline-primary">Sign up for free</button>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card mb-4 rounded-3 shadow-sm">
+					<div class="card-header py-3">
+						<h4 class="my-0 fw-normal">Standard</h4>
+					</div>
+					<div class="card-body">
+						<h1 class="card-title pricing-card-title">$15<small class="text-muted fw-light">/mo</small></h1>
+						<ul class="list-unstyled mt-3 mb-4">
+							<li>20 users included</li>
+							<li>10 GB of storage</li>
+							<li>Priority email support</li>
+							<li>Help center access</li>
+						</ul>
+						<button type="button" class="w-100 btn-lg btn-outline-primary">Get started</button>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card mb-4 rounded-3 shadow-sm">
+					<div class="card-header py-3">
+						<h4 class="my-0 fw-normal">Pro</h4>
+					</div>
+					<div class="card-body">
+						<h1 class="card-title pricing-card-title">$20<small class="text-muted fw-light">/mo</small></h1>
+						<ul class="list-unstyled mt-3 mb-4">
+							<li>20 users included</li>
+							<li>10 GB of storage</li>
+							<li>Priority email support</li>
+							<li>Help center access</li>
+						</ul>
+						<button type="button" class="w-100 btn-lg btn-outline-primary">Get started</button>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card mb-4 rounded-3 shadow-sm border-primary">
+					<div class="card-header py-3 text-white bg-primary border-primary">
+						<h4 class="my-0 fw-normal">Enterprise</h4>
+					</div>
+					<div class="card-body">
+						<h1 class="card-title pricing-card-title">$29<small class="text-muted fw-light">/mo</small></h1>
+						<ul class="list-unstyled mt-3 mb-4">
+							<li>30 users included</li>
+							<li>15 GB of storage</li>
+							<li>Phone and email support</li>
+							<li>Help center access</li>
+						</ul>
+						<button type="button" class="w-100 btn btn-lg btn-primary">Contact us</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
