462bced90a4afc9b1b6e4d3e448cefb89507c5c9
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2023-12-17 13:02:01 +0500

    about pg declutter

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,111 +6,31 @@
 
 <div class="jumbotron">
   <div class="container">
-    <h1>Hello, world!</h1>
+    <h1>About {{ config('app.name') }}</h1>
     <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
     <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
   </div>
 </div>
 
-<div class="container marketing">
+<div class="container marketing pt-5 pb-5">
 
   <!-- Three columns of text below the carousel -->
   <div class="row">
-    <div class="col-lg-4">
-      <i class="fa fa-cloud fa-5x"></i>
-      <h2>Heading</h2>
-      <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-      <p><a class="btn btn-dark" href="#" role="button">View details »</a></p>
-    </div><!-- /.col-lg-4 -->
-    <div class="col-lg-4">
-      <i class="fa fa-server fa-5x"></i>
-      <h2>Heading</h2>
-      <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-      <p><a class="btn btn-dark" href="#" role="button">View details »</a></p>
-    </div><!-- /.col-lg-4 -->
-    <div class="col-lg-4">
-      <i class="fa fa-globe fa-5x"></i>
-      <h2>Heading</h2>
-      <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-      <p><a class="btn btn-dark" href="#" role="button">View details »</a></p>
+    <div class="col-lg-6 col-sm-12">
+      <img src="{{ asset('/images/aboutimg.jpg') }}" width="100%">
+    </div>
+    <div class="col-lg-6">
+      <h2>About Us</h2>
+      <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+      <p><a class="btn btn-primary" href="#" role="button">View details »</a></p>
     </div><!-- /.col-lg-4 -->
   </div><!-- /.row -->
 
-
-  <!-- START THE FEATURETTES -->
-
-  <hr class="featurette-divider">
-
-  <div class="row featurette">
-    <div class="col-md-7">
-      <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-      <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
-    </div>
-    <div class="col-md-5">
-      <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-
-    </div>
-  </div>
-
-  <hr class="featurette-divider">
-
-  <div class="row featurette">
-    <div class="col-md-7 order-md-2">
-      <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-      <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
-    </div>
-    <div class="col-md-5 order-md-1">
-      <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-
-    </div>
-  </div>
-
-  <hr class="featurette-divider">
-
-  <div class="row featurette">
-    <div class="col-md-7">
-      <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-      <p class="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
-    </div>
-    <div class="col-md-5">
-      <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-
-    </div>
-  </div>
-
-  <!-- /END THE FEATURETTES -->
-
 </div>
 <br>
-<section class="partners">
-  <div class="container">
-    <h2 class="text-center text-white">Our Partners</h2>
-    <br>
-    <div class="row">
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo1.png') }}">
-      </div>
 
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo2.png') }}">
-      </div>
+@include('partials.faq')
 
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo3.png') }}">
-      </div>
+@include('partials.partners')
 
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo4.png') }}">
-      </div>
-
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo5.png') }}">
-      </div>
-
-      <div class="col col-lg-2 col-md-6 col-sm-12">
-        <img src="{{ asset('/images/logo6.png') }}">
-      </div>
-    </div>
-  </div>
-</section>
 @endsection
