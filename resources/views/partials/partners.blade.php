0038e214bffa9f313a2199c2509be32d5a36a3bf
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2023-12-17 13:03:12 +0500

    partners partial added

--- resources/views/partials/partners.blade.php
+++ resources/views/partials/partners.blade.php
@@ -1,30 +1,30 @@
 <section class="partners">
 	<div class="container">
-		<h2 class="text-center text-white">Our Partners</h2>
+		<h2 class="text-center">Our Partners</h2>
 		<br>
 		<div class="row">
 			<div class="col col-lg-2 col-md-6 col-sm-12">
-				<img src="{{ asset('/images/logo1.png') }}">
+				<img src="{{ asset('/images/logo1.png') }}" class="img-responsive">
 			</div>
 
 			<div class="col col-lg-2 col-md-6 col-sm-12">
-				<img src="{{ asset('/images/logo2.png') }}">
+				<img src="{{ asset('/images/logo2.png') }}" class="img-responsive">
 			</div>
 
 			<div class="col col-lg-2 col-md-6 col-sm-12">
-				<img src="{{ asset('/images/logo3.png') }}">
+				<img src="{{ asset('/images/logo3.png') }}" class="img-responsive">
 			</div>
 
 			<div class="col col-lg-2 col-md-6 col-sm-12">
-				<img src="{{ asset('/images/logo4.png') }}">
+				<img src="{{ asset('/images/logo4.png') }}" class="img-responsive">
 			</div>
 
 			<div class="col col-lg-2 col-md-6 col-sm-12">
-				<img src="{{ asset('/images/logo5.png') }}">
+				<img src="{{ asset('/images/logo5.png') }}" class="img-responsive">
 			</div>
 
 			<div class="col col-lg-2 col-md-6 col-sm-12">
-				<img src="{{ asset('/images/logo6.png') }}">
+				<img src="{{ asset('/images/logo6.png') }}" class="img-responsive">
 			</div>
 		</div>
 	</div>
