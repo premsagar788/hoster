0adeca8531350c9c030c6275c0de9faae6aba6ab
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2023-12-19 18:37:18 +0500

    pricing update

--- resources/views/partials/pricing.blade.php
+++ resources/views/partials/pricing.blade.php
@@ -7,17 +7,17 @@
 			<div class="col">
 				<div class="card mb-4 rounded-3 shadow-sm">
 					<div class="card-header py-3">
-						<h4 class="my-0 fw-normal">Free</h4>
+						<h4 class="my-0 fw-normal">Starter</h4>
 					</div>
 					<div class="card-body">
-						<h1 class="card-title pricing-card-title">$0<small class="text-muted fw-light">/mo</small></h1>
+						<h1 class="card-title pricing-card-title">$3<small class="text-muted fw-light">/mo</small></h1>
 						<ul class="list-unstyled mt-3 mb-4">
-							<li>10 users included</li>
-							<li>2 GB of storage</li>
-							<li>Email support</li>
-							<li>Help center access</li>
+							<li class="icon-fade"><span class="fa fa-hdd-o"></span> 2 GB storage</li>
+							<li class="icon-fade"><span class="fa fa-tachometer"></span> 3 GB bandwidth</li>
+							<li class="icon-fade"><span class="fa fa-envelope"></span> 5 email accounts</li>
+							<li class="icon-fade"><span class="fa fa-support"></span> Email support</li>
 						</ul>
-						<button type="button" class="w-100 btn-lg btn-outline-primary">Sign up for free</button>
+						<a href="{{ url('/domains') }}" class="btn btn-outline-primary btn-lg w-100">Get started <span class="fa fa-rocket"></span></a>
 					</div>
 				</div>
 			</div>
@@ -27,14 +27,14 @@
 						<h4 class="my-0 fw-normal">Standard</h4>
 					</div>
 					<div class="card-body">
-						<h1 class="card-title pricing-card-title">$15<small class="text-muted fw-light">/mo</small></h1>
+						<h1 class="card-title pricing-card-title">$8<small class="text-muted fw-light">/mo</small></h1>
 						<ul class="list-unstyled mt-3 mb-4">
-							<li>20 users included</li>
-							<li>10 GB of storage</li>
-							<li>Priority email support</li>
-							<li>Help center access</li>
+							<li class="icon-fade"><span class="fa fa-hdd-o"></span> 5 GB storage</li>
+							<li class="icon-fade"><span class="fa fa-tachometer"></span> 8 GB bandwidth</li>
+							<li class="icon-fade"><span class="fa fa-envelope"></span> 10 email accounts</li>
+							<li class="icon-fade"><span class="fa fa-support"></span> Email support</li>
 						</ul>
-						<button type="button" class="w-100 btn-lg btn-outline-primary">Get started</button>
+						<a href="{{ url('/domains') }}" class="btn btn-outline-primary btn-lg w-100">Get started <span class="fa fa-rocket"></span></a>
 					</div>
 				</div>
 			</div>
@@ -46,12 +46,12 @@
 					<div class="card-body">
 						<h1 class="card-title pricing-card-title">$20<small class="text-muted fw-light">/mo</small></h1>
 						<ul class="list-unstyled mt-3 mb-4">
-							<li>20 users included</li>
-							<li>10 GB of storage</li>
-							<li>Priority email support</li>
-							<li>Help center access</li>
+							<li class="icon-fade"><span class="fa fa-hdd-o"></span> 10 GB storage</li>
+							<li class="icon-fade"><span class="fa fa-tachometer"></span> 15 GB bandwidth</li>
+							<li class="icon-fade"><span class="fa fa-envelope"></span> 20 email accounts</li>
+							<li class="icon-fade"><span class="fa fa-support"></span> Priority Email support</li>
 						</ul>
-						<button type="button" class="w-100 btn-lg btn-outline-primary">Get started</button>
+						<a href="{{ url('/domains') }}" class="btn btn-outline-primary btn-lg w-100">Get started <span class="fa fa-rocket"></span></a>
 					</div>
 				</div>
 			</div>
@@ -63,12 +63,12 @@
 					<div class="card-body">
 						<h1 class="card-title pricing-card-title">$29<small class="text-muted fw-light">/mo</small></h1>
 						<ul class="list-unstyled mt-3 mb-4">
-							<li>30 users included</li>
-							<li>15 GB of storage</li>
-							<li>Phone and email support</li>
-							<li>Help center access</li>
+							<li class="icon-fade"><span class="fa fa-hdd-o"></span> Unlimited storage</li>
+							<li class="icon-fade"><span class="fa fa-tachometer"></span> Unlimited bandwidth</li>
+							<li class="icon-fade"><span class="fa fa-envelope"></span> Unlimited email accounts</li>
+							<li class="icon-fade"><span class="fa fa-support"></span> Priority Email support</li>
 						</ul>
-						<button type="button" class="w-100 btn btn-lg btn-primary">Contact us</button>
+						<button type="button" class="w-100 btn btn-lg btn-primary">Contact us <span class="fa fa-envelope"></button>
 					</div>
 				</div>
 			</div>
